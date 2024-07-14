6f7e0280ac8b4351c37eb45ac5032d4dab01b991
이진국 <jinkook0126@naver.com>
2024-07-14 22:24:28 +0900

    feat: 탭 UI 변경

--- src/app/_component/ListContainer.tsx
+++ src/app/_component/ListContainer.tsx
@@ -1,18 +1,27 @@
 "use client";
-import { Stack, Box, Tabs, TabList, Tab, TabPanels } from "@chakra-ui/react";
+import { Stack, Box, Flex, Text, Center } from "@chakra-ui/react";
 import ListCard from "./ListCard";
 import { useEffect, useState } from "react";
 import { collection, query, orderBy, onSnapshot } from "firebase/firestore";
 import firestore from "@/firebase/firestore";
 import { FoodItem } from "@/types/mucket";
 
+type Ttag = "eunji" | "jongyoon";
+type TTagMap = {
+  label: string;
+  value: Ttag;
+};
+
 const ListContainer = () => {
   const [lists, setLists] = useState<FoodItem[]>([]);
-  const tags = ["jongyoon", "eunji"];
-  const [tabIndex, setTabIndex] = useState<number>(0);
+  const [tab, setTab] = useState<Ttag>("jongyoon");
+  const tags: TTagMap[] = [
+    { label: "종윤", value: "jongyoon" },
+    { label: "은지", value: "eunji" },
+  ];
 
-  const handleTabsChange = (v: number) => {
-    setTabIndex(v);
+  const handleTabsChange = (v: Ttag) => {
+    setTab(v);
   };
   useEffect(() => {
     const getData = async () => {
@@ -32,20 +41,28 @@ const ListContainer = () => {
   }, []);
   return (
     <Box p={6} h="100vh" bg={"gray.200"}>
-      <Tabs
-        variant="soft-rounded"
-        colorScheme="green"
-        isFitted
-        onChange={handleTabsChange}
-      >
-        <TabList>
-          <Tab>종윤</Tab>
-          <Tab>은지</Tab>
-        </TabList>
-      </Tabs>
+      <Flex alignItems={"center"} gap={4} w={"100%"} justify={"center"}>
+        {tags.map((item) => (
+          <Center
+            key={item.value}
+            onClick={() => handleTabsChange(item.value)}
+            flex={1}
+            bg={tab === item.value ? "green.100" : "gray.300"}
+            h={10}
+            borderRadius={"full"}
+          >
+            <Text
+              fontWeight={700}
+              color={tab === item.value ? "green.700" : "gray.600"}
+            >
+              {item.label}
+            </Text>
+          </Center>
+        ))}
+      </Flex>
       <Stack spacing={4} mt={4}>
         {lists
-          .filter((item) => item.tag === tags[tabIndex])
+          .filter((item) => item.tag === tab)
           .map((item) => (
             <ListCard key={item.id} {...item} />
           ))}
